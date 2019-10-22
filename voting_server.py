03051e316623a8f25c929bb71e2973352d69815a
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-10-23 02:53:26 +0300

    Updated voting_server.py

--- voting_server.py
+++ voting_server.py
@@ -12,18 +12,17 @@ vote_list_encrypted = []
 client_count = 0
 
 # Receive encrypted_choices from client
-while client_count < 2:     # MODIFY CLIENT COUNT LATER
+while client_count < 2:     # MODIFY CLIENT COUNT
     conn, addr = serv.accept()
     print("Connection from client: ", addr)
     msg = conn.recv(1024)
     encrypted_choices = msg.decode("ascii")
-    print(encrypted_choices)
     # add to accumulator
     vote_list_encrypted.append(encrypted_choices)
     client_count += 1
     conn.close()
 
 
-print(vote_list_encrypted)
+print(encrypted_choices)
 # Send the sum to Trustee Server
 
