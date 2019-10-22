5b855b83a06a2e8dbd78caebdbf7499012a7a4ce
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-10-23 00:45:22 +0300

    Added JSON serialization to public key

--- client.py
+++ client.py
@@ -1,20 +1,17 @@
 import socket
+import json
 from phe import paillier
 
 # create a socket object
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # get local machine name
 host = socket.gethostname()                           
 portTrustee = 12345
-
 # connection to hostname on the port.
 clientSocket.connect((host, portTrustee))                               
 # Receive no more than 1024 bytes
 msg = clientSocket.recv(1024)                                
 public_key = msg.decode("ascii")
-print(public_key)
-
 clientSocket.close()
 
 # Client needs to vote
@@ -22,26 +19,40 @@ candidates = ['Donald Trump', 'Roger Federer', 'Britney Spears', 'Ali El Deek','
 
 print("Choose your candidates (up to 2) among the list: "  + str(candidates))
 
+# print(public_key)            
+
+# Deserialize the public key
+received_dict = json.loads(public_key)
+pk = received_dict['public_key']
+public_key_rec = paillier.PaillierPublicKey(n=int(pk['n']))
+print(str(public_key_rec))
+
 
 vote_count = 0
 # choice is 0 by default so it can compute to 0 when encoded
 choice = "-1"
 summed_choices = 0
 
-while(choice != "end" and vote_count < 2):
+while(vote_count < 2):
     print("0 - Donald Trump")
     print("1 - Roger Federer")
     print("2 - Britney Spears")
     print("3 - Ali El Deek")
     print("4 - Steve Jobs")
-    print("end to exit")
+    print("9  to exit")
     choice = int(input())
     #encode the choice
+    if(choice == 9):
+        break
     summed_choices += pow(10, choice)
     vote_count += 1 
 
 print("summed choices " + str(summed_choices))
 
+# now we encrypt with it the summed choices
+encrypted_choices = public_key_rec.encrypt(summed_choices)
 
+print(encrypted_choices)
 
-#discussions here pls 
+# discussions here pls   
+
--- trustee.py
+++ trustee.py
@@ -1,12 +1,13 @@
 import socket
+import json
 from phe import paillier
 
 public_key, private_key = paillier.generate_paillier_keypair()
-secret_list = [21, 34, 231]
-print(secret_list)
-encrypted_list = [public_key.encrypt(x) for x in secret_list]
-print(encrypted_list)
-print([private_key.decrypt(x) for x in encrypted_list])
+# secret_list = [21, 34, 231]
+# print(secret_list)
+# encrypted_list = [public_key.encrypt(x) for x in secret_list]
+# print(encrypted_list)
+# print([private_key.decrypt(x) for x in encrypted_list])
 
 # Encryption works
 
@@ -17,9 +18,16 @@ port = 12345
 serv.bind((host, port))
 serv.listen(5)
 
+# serialize to json
+dict_public_key = {}
+dict_public_key['public_key'] =  {'n': public_key.n}
+serialized_public_key = json.dumps(dict_public_key)
+
+print(serialized_public_key)
 
 while True:
     conn, addr = serv.accept()
-    print("Connection from client", addr)
-    conn.send(str(public_key).encode())
+    print("Connection from client: ", addr)
+    # cast to string then encode into bytes
+    conn.sendall(str(serialized_public_key).encode())
     conn.close()
