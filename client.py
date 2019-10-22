9be92297f747991ba5f1eea8f710a22bdffebd50
MarwanNour <38836519+MarwanNour@users.noreply.github.com>
2019-10-22 21:46:35 +0300

    Added Encoding

--- client.py
+++ client.py
@@ -12,8 +12,34 @@ portTrustee = 12345
 clientSocket.connect((host, portTrustee))                               
 # Receive no more than 1024 bytes
 msg = clientSocket.recv(1024)                                
+public_key = msg.decode("ascii")
+print(public_key)
 
-print(msg.decode('ascii'))
 clientSocket.close()
 
-#discussions here pls 
+# Client needs to vote
+candidates = ['Donald Trump', 'Roger Federer', 'Britney Spears', 'Ali El Deek','Steve Jobs']
+
+print("Choose your candidates (up to 2) among the list: "  + candidates)
+
+
+vote_count = 0
+# choice is 0 by default so it can compute to 0 when encoded
+choice = "-1"
+summed_choices = 0
+
+while(choice != "end" and vote_count >= 2):
+    print("0 - Donald Trump")
+    print("1 - Roger Federer")
+    print("2 - Britney Spears")
+    print("3 - Ali El Deek")
+    print("4 - Steve Jobs")
+    print("end to exit")
+    choice = int(input())
+    #encode the choice
+    summed_choices += 10^choice
+    vote_count += 1 
+
+
+#discussions here pls
+
